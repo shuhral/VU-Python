f4f4e271129fa493dc32faa8d6cd3d1089266817
shuhral <mail@shahrulazri.com>
2022-01-21 17:12:48 +1100

    Initial commit

---
+++ Practice01.py
@@ -0,0 +1,20 @@
+myList = []
+x = 0
+
+# Accept an integer as input() five times.
+while x < 5:
+    myInput = int(input("Enter a number: "))
+    myList.append(myInput)
+    x = x + 1
+
+# Display the length of the list.    
+print(len(myList))
+
+# Display the first and the last integers in the list.
+print(myList[0], myList[-1])
+
+# Add the integers in the list.
+totalSum = 0
+for i in range(len(myList)):
+    totalSum += myList[i]
+print(totalSum)
