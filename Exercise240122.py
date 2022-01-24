75617e7234dea2f317b573a6006f7d6640bb40fd
shuhral <mail@shahrulazri.com>
2022-01-24 19:04:58 +1100

    Part C
    Add formatting to option 2.

--- Exercise240122.py
+++ Exercise240122.py
@@ -14,17 +14,16 @@ if option == 1:
     myFile.write(myString)
     myFile.close()
 elif option == 2:
+# Part B
+# Add a check under option 2 for the existence of the file. If the file does not exist, it should be created.
     try:
         myFile = open("file.txt", "r")
-        print(myFile.read())
+        readFile = myFile.read()
+# Part C
+# Add formatting to option 2 such that the text is center aligned in an 80 character window.
+        print(readFile.center(80))
     except FileNotFoundError:
         print("File not found. Creating file.")
         myFile = open("file.txt", "x")
 else:
     print("That is not a valid option. Try again.")
-
-# Part B
-# Add a check under option 2 for the existence of the file. If the file does not exist, it should be created.
-
-# Part C
-# Add formatting to option 2 such that the text is center aligned in an 80 character window.
